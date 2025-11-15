083aa362319c663cf6daa018d78441bcb621bbf8
Cameron Koehler <camekommd@gmail.com>
2025-11-15 00:07:52 -0700

    Pulling repo, automatic make, run

--- run.sh
+++ run.sh
@@ -18,10 +18,25 @@ echo "Installing x11 development libraries"
 sudo apt install -y libx11-dev libxt-dev libxext-dev libxrandr-dev libxinerama-dev
 
 echo "Installing Xorg and some apps, if haven't already"
-sudo apt install -y xorg x11-apps
+sudo apt install -y xorg x11-apps xterm
 
 echo "Installing Motif/OpenMotif development libraries"
 sudo apt install -y libmotif-dev libxm4
 
 echo "Installing helpful tools for grabbing repo"
 sudo apt install -y git curl
+
+
+echo "Cloning repo.."
+git clone https://github.com/alx210/emwm.git
+
+echo "Proceeding into dir, making and hopefully running"
+cd emwm
+sudo make install
+
+clear
+sleep 1
+echo "Install finished. Attempting to start DE. Exit by going into different tty"
+sleep 1
+xterm &
+startx /usr/bin/emwm
